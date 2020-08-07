ae9e386e6bc60de1c548158e1fb99251da97c557
williamspiro <willspiro@gmail.com>
2020-08-07 11:59:42 -0400

    Add modules

---
+++ modules/B4BL Donation Status.module/module.js
@@ -0,0 +1,8 @@
+$('.mydata:gt(3)').hide().last().after(
+    $('<a />').attr('href','#').text('Show more').click(function(){
+        var a = this;
+        $('.mydata:not(:visible):lt(5)').fadeIn(function(){
+         if ($('.mydata:not(:visible)').length == 0) $(a).remove();   
+        }); return false;
+    })
+);
